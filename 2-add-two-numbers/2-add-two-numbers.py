034a618ec7f29232f18dfac910ae85cc7c6877b8
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-15 00:02:24 +0100

    Time: 114 ms (40.15%), Space: 14 MB (43.79%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -4,28 +4,42 @@ class ListNode:
         self.val = val
         self.next = next
         
-def convert_list_node_to_num(listNode):
+def convert_list_node_to_num(list_node):
     result = 0
     power_of_10 = 0
     while True:
-        result = (listNode.val * (10 ** power_of_10)) + result
+        result = (list_node.val * (10 ** power_of_10)) + result
         power_of_10 += 1
-        listNode = listNode.next
-        if not listNode:
+        list_node = list_node.next
+        if not list_node:
             break
     return result
 
-def stringToListNode(string):
-    string = str(string)
-    result = None
-    for i in string:
-        result = ListNode(i, result)
-    return result
+def convert_num_to_list_node(num):
+    print(str(num)[-1])
+    if num == 0:
+        return ListNode(0, None)
+    num = int(num)
+    return ListNode(num % 10, convert_num_to_list_node(num / 10)) if num >= 1 else None
+    
             
     
     
 class Solution:
+    CARRY = 0
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        result = convert_list_node_to_num(l1) + convert_list_node_to_num(l2)
-        return stringToListNode(result)
+        if l1 or l2 or self.CARRY:
+            _sum = 0 + self.CARRY
+            if l1 and l1.val:
+                _sum += l1.val
+            if l2 and l2.val:
+                _sum += l2.val
+            self.CARRY = int(_sum / 10)
+            if self.CARRY > 0:
+                _sum = _sum % 10
+            l1_next = l1 and l1.next
+            l2_next = l2 and l2.next
+            result = ListNode(_sum, self.addTwoNumbers(l1_next, l2_next))
+            return result
+        return None
         
