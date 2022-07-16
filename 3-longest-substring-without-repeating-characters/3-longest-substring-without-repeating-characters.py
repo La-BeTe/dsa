3ca75a56372a15d1f20b7c5bc6ee84c4a3011684
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-17 00:55:00 +0100

    Time: 709 ms (11.67%), Space: 14 MB (49.30%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,21 +1,35 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        # left = 0
+        # right = 0
+        # longest_substr_length = 0
+        # hash_map = {}
+        # while True:
+        #     if right >= len(s):
+        #         longest_substr_length = max(longest_substr_length, right - left)
+        #         break
+        #     char = s[right]
+        #     if char not in hash_map:
+        #         hash_map[char] = right
+        #         right += 1
+        #     else:
+        #         longest_substr_length = max(longest_substr_length, right - left)
+        #         left = hash_map[char] + 1
+        #         right = left
+        #         hash_map = {}
+        # return longest_substr_length
         left = 0
-        right = 0
-        longest_substr_length = 0
-        hash_map = {}
-        while True:
-            if right >= len(s):
-                longest_substr_length = max(longest_substr_length, right - left)
-                break
-            char = s[right]
-            if char not in hash_map:
-                hash_map[char] = right
-                right += 1
-            else:
-                longest_substr_length = max(longest_substr_length, right - left)
-                left = hash_map[char] + 1
-                right = left
-                hash_map = {}
-        return longest_substr_length
+        longest_substr_length = -inf
+
+        while left < len(s):
+            hash_map = {}
+            ptr = left
+            while s[ptr] not in hash_map:
+                hash_map[s[ptr]] = 1
+                ptr += 1
+                if ptr == len(s):
+                    break
+            longest_substr_length = max(longest_substr_length, ptr - left)
+            left += 1
+        return longest_substr_length if longest_substr_length != -inf else 0
             
