8de798975120a2dca9ea3faadb1d9294552ee022
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-17 23:36:59 +0100

    Time: 703 ms (11.80%), Space: 98.7 MB (5.00%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,20 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring =     function (str) {
-      let longest = 0;
-      let seen = {};
-      let start = 0;
-     
-      for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        if (seen[char]) {
-          start = Math.max(start, seen[char]);
+var lengthOfLongestSubstring =     function (s){
+    // add whatever parameters you deem necessary - good luck!
+    let left = 0;
+    let longest_substr_length = 0;
+    while(left < s.length){
+        const hash_map = {};
+        let ptr = left;
+        while(!hash_map[s[ptr]]){
+            hash_map[s[ptr]] = 1;
+            ptr += 1;
+            if(ptr == s.length){
+                break;
+            }
         }
-        // index - beginning of substring + 1 (to include current in count)
-        longest = Math.max(longest, i - start + 1);
-        // store the index of the next char so as to not double count
-        seen[char] = i + 1;
-      }
-      return longest;
+        longest_substr_length = Math.max(longest_substr_length, ptr - left);
+        left += 1;
     }
+    return longest_substr_length;
+}
