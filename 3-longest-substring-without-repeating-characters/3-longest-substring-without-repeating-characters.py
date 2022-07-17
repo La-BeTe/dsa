1b057b9675f46a3c60a3fc440401ec46d6b34e11
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-17 16:16:26 +0100

    Time: 1061 ms (7.62%), Space: 13.9 MB (92.90%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -2,6 +2,8 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left = 0
         longest_substr_length = 0
+        stop_loop_flag = False
+
         while left < len(s):
             hash_map = {}
             ptr = left
@@ -9,8 +11,11 @@ class Solution:
                 hash_map[s[ptr]] = 1
                 ptr += 1
                 if ptr == len(s):
+                    stop_loop_flag = True
                     break
             longest_substr_length = max(longest_substr_length, ptr - left)
             left += 1
+            if stop_loop_flag:
+                break
         return longest_substr_length
             
