71806e7a6356beea279087f5c7c13a641a411ea0
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-24 04:57:58 +0100

    Time: 1288 ms (65.26%), Space: 14.1 MB (19.42%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,6 +1,8 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         s_len = len(s)
+        if s_len < 2:
+            return s
         hash_map = {}
         for i, char in enumerate(s):
             hash_map[char] = [] if char not in hash_map else hash_map[char]
@@ -8,25 +10,34 @@ class Solution:
         start, hash_map_ptr = 0, -1
         end, longest_palindrome, longest_palindrome_len = hash_map[s[start]][hash_map_ptr], '', 0
         while start < s_len:
-            end += 1
+            can_move_start = False
+            should_break = False
             if longest_palindrome_len > (s_len - start):
                 break
-            if longest_palindrome_len > (end - start):
-                start += 1
-                hash_map_ptr = -1
-                end = hash_map[s[start]][hash_map_ptr]
-                continue
-            sliced_s = s[start:end]
-            reversed_s = sliced_s[::-1]
-            if sliced_s == reversed_s:
-                longest_palindrome = reversed_s
-                longest_palindrome_len = len(reversed_s)
-                start += 1
+            end += 1
+            if s[start] in hash_map and ((hash_map[s[start]][-1] - hash_map[s[start]][0]) < longest_palindrome_len):
+                del hash_map[s[start]]
+                can_move_start = True
+            elif s[start] not in hash_map or (len(hash_map[s[start]]) + hash_map_ptr) < 0:
+                can_move_start = True
+            else:
+                sliced_s = s[start:end]
+                reversed_s = sliced_s[::-1]
+                if sliced_s == reversed_s:
+                    can_move_start = True
+                    if len(reversed_s) > longest_palindrome_len:
+                        longest_palindrome = reversed_s
+                        longest_palindrome_len = len(reversed_s)
+            if can_move_start:
                 hash_map_ptr = 0
-            elif (len(hash_map[s[start]]) + hash_map_ptr) < 0:
-                start += 1
-                hash_map_ptr = 0
-            if start >= s_len:
+                while True:
+                    start += 1
+                    if start >= s_len:
+                        should_break = True
+                        break
+                    if s[start] in hash_map:
+                        break
+            if should_break:
                 break
             hash_map_ptr -= 1
             end = hash_map[s[start]][hash_map_ptr]
