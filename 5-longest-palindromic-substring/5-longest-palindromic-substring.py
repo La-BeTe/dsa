e5b5cbc492ba42da486f4742e81d1b9150f26212
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-24 05:07:12 +0100

    Time: 2174 ms (29.59%), Space: 14.3 MB (16.41%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,8 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         s_len = len(s)
-        if s_len < 2:
-            return s
         hash_map = {}
         for i, char in enumerate(s):
             hash_map[char] = [] if char not in hash_map else hash_map[char]
@@ -10,11 +8,11 @@ class Solution:
         start, hash_map_ptr = 0, -1
         end, longest_palindrome, longest_palindrome_len = hash_map[s[start]][hash_map_ptr], '', 0
         while start < s_len:
-            can_move_start = False
-            should_break = False
             if longest_palindrome_len > (s_len - start):
                 break
             end += 1
+            can_move_start = False
+            should_break = False
             if s[start] in hash_map and ((hash_map[s[start]][-1] - hash_map[s[start]][0]) < longest_palindrome_len):
                 del hash_map[s[start]]
                 can_move_start = True
