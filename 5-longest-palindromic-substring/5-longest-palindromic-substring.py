a8588c7b6a95efaca8e403f9f6aae7374cb79290
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-24 03:43:29 +0100

    Time: 1500 ms (53.28%), Space: 14.1 MB (29.50%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,24 +1,36 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         s_len = len(s)
-        start, end = 0, s_len
-        longest_palindrome = ''
-        longest_palindrome_len = 0
+        hash_map = {}
+        for i, char in enumerate(s):
+            hash_map[char] = [] if char not in hash_map else hash_map[char]
+            hash_map[char].append(i)
+        start, end, longest_palindrome, longest_palindrome_len = 0, hash_map[s[0]][-1] + 1, '', 0
+        hash_map_ptr = -1
+        # print(hash_map['p'])
         while start < s_len:
-            if s[start] != s[end-1]:
-                end -= 1
-                continue
+            # print(start, end, longest_palindrome, hash_map_ptr)
             if longest_palindrome_len > (s_len - start):
                 break
             if longest_palindrome_len > (end - start):
                 start += 1
-                end = s_len
+                hash_map_ptr = -1
+                end = hash_map[s[start]][hash_map_ptr] + 1
                 continue
             sliced_s = s[start:end]
             reversed_s = sliced_s[::-1]
+            # print(sliced_s, reversed_s)
             if sliced_s == reversed_s:
                 longest_palindrome = reversed_s
                 longest_palindrome_len = len(reversed_s)
-            end -= 1
+                start += 1
+                hash_map_ptr = 0
+            elif (len(hash_map[s[start]]) + hash_map_ptr) < 0:
+                start += 1
+                hash_map_ptr = 0
+            if start >= s_len:
+                break
+            hash_map_ptr -= 1
+            end = hash_map[s[start]][hash_map_ptr] + 1
         return longest_palindrome
                 
