53237d0dfc85460c5f4355e5095306a52e68a906
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-24 03:49:02 +0100

    Time: 1406 ms (58.68%), Space: 14.2 MB (19.42%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -5,21 +5,19 @@ class Solution:
         for i, char in enumerate(s):
             hash_map[char] = [] if char not in hash_map else hash_map[char]
             hash_map[char].append(i)
-        start, end, longest_palindrome, longest_palindrome_len = 0, hash_map[s[0]][-1] + 1, '', 0
-        hash_map_ptr = -1
-        # print(hash_map['p'])
+        start, hash_map_ptr = 0, -1
+        end, longest_palindrome, longest_palindrome_len = hash_map[s[start]][hash_map_ptr], '', 0
         while start < s_len:
-            # print(start, end, longest_palindrome, hash_map_ptr)
+            end += 1
             if longest_palindrome_len > (s_len - start):
                 break
             if longest_palindrome_len > (end - start):
                 start += 1
                 hash_map_ptr = -1
-                end = hash_map[s[start]][hash_map_ptr] + 1
+                end = hash_map[s[start]][hash_map_ptr]
                 continue
             sliced_s = s[start:end]
             reversed_s = sliced_s[::-1]
-            # print(sliced_s, reversed_s)
             if sliced_s == reversed_s:
                 longest_palindrome = reversed_s
                 longest_palindrome_len = len(reversed_s)
@@ -31,6 +29,6 @@ class Solution:
             if start >= s_len:
                 break
             hash_map_ptr -= 1
-            end = hash_map[s[start]][hash_map_ptr] + 1
+            end = hash_map[s[start]][hash_map_ptr]
         return longest_palindrome
                 
