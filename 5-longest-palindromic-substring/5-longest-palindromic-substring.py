a3897398549d471815061a3f107e409f4b833516
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-24 05:16:58 +0100

    Time: 1292 ms (65.10%), Space: 14.2 MB (16.41%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,6 +1,8 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         s_len = len(s)
+        if s_len < 2:
+            return s
         hash_map = {}
         for i, char in enumerate(s):
             hash_map[char] = [] if char not in hash_map else hash_map[char]
@@ -8,19 +10,15 @@ class Solution:
         start, hash_map_ptr = 0, -1
         end, longest_palindrome, longest_palindrome_len = hash_map[s[start]][hash_map_ptr], '', 0
         while start < s_len:
+            can_move_start = False
+            should_break = False
             if longest_palindrome_len > (s_len - start):
                 break
             end += 1
-            can_move_start = False
-            should_break = False
-            if s[start] not in hash_map:
-                can_move_start = True
-            elif longest_palindrome_len > (end - start):
-                can_move_start = True
-            elif (len(hash_map[s[start]]) + hash_map_ptr) < 0:
-                can_move_start = True
-            elif s[start] in hash_map and ((hash_map[s[start]][-1] - hash_map[s[start]][0]) < longest_palindrome_len):
+            if s[start] in hash_map and ((hash_map[s[start]][-1] - hash_map[s[start]][0]) < longest_palindrome_len):
                 del hash_map[s[start]]
+                can_move_start = True
+            elif s[start] not in hash_map or (len(hash_map[s[start]]) + hash_map_ptr) < 0:
                 can_move_start = True
             else:
                 sliced_s = s[start:end]
@@ -44,4 +42,3 @@ class Solution:
             hash_map_ptr -= 1
             end = hash_map[s[start]][hash_map_ptr]
         return longest_palindrome
-                
