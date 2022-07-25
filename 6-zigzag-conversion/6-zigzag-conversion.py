e16cfd1616ca80646a64dd508a177e00d4a7658c
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-26 00:52:12 +0100

    Time: 117 ms (30.80%), Space: 13.9 MB (96.00%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -4,9 +4,9 @@ class Solution:
         ptr = 0
         is_going_backwards = False
         for i in range(numRows):
-            li.append([])
+            li.append('')
         for i, char in enumerate(s):
-            li[ptr].append(char)
+            li[ptr] += char
             if (i == numRows) or (ptr == numRows - 1):
                 ptr -= 1
                 is_going_backwards = True
@@ -15,10 +15,7 @@ class Solution:
             else:
                 ptr += 1
             if ptr == -1:
-                ptr = 1 if len(li) > 1 else 0
+                ptr = 1 if numRows > 1 else 0
                 is_going_backwards = False
-        result = ''
-        for row in li:
-            result += ''.join(row)
-        return result
+        return ''.join(li)
             
