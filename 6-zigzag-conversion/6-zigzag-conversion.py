5d88fac8a14da638439441b4cbc5a1a5c707cdfb
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-26 01:19:37 +0100

    Time: 65 ms (86.87%), Space: 14.1 MB (29.40%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -1,19 +1,19 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        li = [''] * numRows
+        li = [[] for _ in range(numRows)]
         ptr = 0
         is_going_backwards = False
         for i, char in enumerate(s):
-            li[ptr] += char
-            if (i == numRows) or (ptr == numRows - 1):
+            li[ptr].append(char)
+            if (i == numRows) or (ptr == numRows - 1) or is_going_backwards:
                 ptr -= 1
                 is_going_backwards = True
-            elif is_going_backwards:
-                ptr -= 1
             else:
                 ptr += 1
             if ptr == -1:
                 ptr = 1 if numRows > 1 else 0
                 is_going_backwards = False
-        return ''.join(li)
-            
+        result = ''
+        for row in li:
+            result += ''.join(row)
+        return result
