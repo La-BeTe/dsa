3a9d96d046e2e2bf8acb44d8aa9c3de68285775b
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-26 03:00:48 +0100

    Time: 36 ms (88.87%), Space: 13.8 MB (96.86%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,11 +1,10 @@
 class Solution:
     def reverse(self, x: int) -> int:
         result = 0
-        abs_x = abs(x) or 1
+        abs_x = abs(x) or 1 # if x is 0, use 1 so we don't get ZeroDivisionError
         is_negative = x // abs_x
         while abs_x >= 1:
-            r = abs_x % 10
-            result = (result * 10) + r
+            result = (result * 10) + (abs_x % 10)
             abs_x = abs_x // 10
         return result * is_negative if result < 2 ** 31 else 0
         
