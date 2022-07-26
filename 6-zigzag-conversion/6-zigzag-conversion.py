9da08e218f7c8c425a934041f65d81d3aed5d26a
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-26 01:49:14 +0100

    Time: 70 ms (81.44%), Space: 14 MB (76.17%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -4,11 +4,8 @@ class Solution:
         ptr, is_going_backwards = 0, False
         for i, char in enumerate(s):
             li[ptr] += char
-            if (i == numRows) or (ptr == numRows - 1):
-                is_going_backwards = True
-                
-            ptr += -1 if is_going_backwards else 1
-                
+            is_going_backwards = is_going_backwards or (i == numRows) or (ptr == numRows - 1)
+            ptr += -1 if is_going_backwards else 1    
             if ptr == -1:
                 ptr = 1 if numRows > 1 else 0
                 is_going_backwards = False
