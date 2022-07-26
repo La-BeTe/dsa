40710e0ca34e93b525309573dea5fb9168a29f4b
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-26 03:14:22 +0100

    Time: 47 ms (60.81%), Space: 13.9 MB (16.00%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -3,8 +3,11 @@ class Solution:
         result = 0
         abs_x = abs(x) or 1 # if x is 0, use 1 so we don't get ZeroDivisionError
         is_negative = x // abs_x
+        limit = 2 ** 31
         while abs_x >= 1:
             result = (result * 10) + (abs_x % 10)
+            if result > limit:
+                return 0
             abs_x = abs_x // 10
-        return result * is_negative if result < 2 ** 31 else 0
+        return result * is_negative
         
