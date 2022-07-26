28f4dbd05785ba8582111178bb040b339da0bd36
Emejuru Emmanuel <66489884+La-BeTe@users.noreply.github.com>
2022-07-27 00:47:11 +0100

    Time: 42 ms (81.48%), Space: 13.8 MB (98.82%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -3,22 +3,23 @@ class Solution:
         sign = num = 0
         limit = (2 ** 31) - 1
         for char in s:
-            is_num = ord(char) >= 48 and ord(char) <= 57
             if sign == 0:
-                if char == '-':
+                if char == ' ':
+                    continue
+                elif char == '-':
                     sign = -1
                     limit += 1
                 elif char == '+':
                     sign = 1
-                elif char == ' ':
-                    continue
                 else:
-                    sign = 1
+                    sign = 1  
+                    is_num = ord(char) >= 48 and ord(char) <= 57
                     if is_num:
                         num += int(char)
                     else:
                         break
-            else:
+            else:    
+                is_num = ord(char) >= 48 and ord(char) <= 57
                 if is_num:
                     num = (num * 10) + int(char)
                 else:
