6e0902dad186816fbfd8b6ade325764f1f5723e4
Emmanuel Emejuru <66489884+La-BeTe@users.noreply.github.com>
2025-03-13 22:50:28 +0100

    Time: 31 ms (97.32%), Space: 28.5 MB (67.48%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,13 +3,17 @@ class Solution:
         i, j, result = 0, len(height) - 1, 0
         while i < j:
             length_of_container, breadth_of_container = 0, j - i
+            
             if height[i] > height[j]:
                 length_of_container = height[j]
                 j -= 1
             else:
                 length_of_container = height[i]
                 i += 1
-            result = max(result, length_of_container * breadth_of_container)
+
+            area = length_of_container * breadth_of_container
+            if area > result:
+                result = area
         return result
 
         
