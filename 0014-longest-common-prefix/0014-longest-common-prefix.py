911fc7148cb72901203614c338b17b5c72156cea
Emmanuel Emejuru <66489884+La-BeTe@users.noreply.github.com>
2025-03-25 01:36:09 +0100

    Time: 3 ms (28.09%), Space: 18 MB (30.74%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,8 +1,8 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         res = strs[0]
-        res_index = None
         res_len = len(res)
+        res_index = res_len
 
         for str in strs[1:]:
             i = 0
@@ -14,7 +14,7 @@ class Solution:
             if i == 0:
                 return ""
                 
-            if res_index is None or i < res_index:
+            if i < res_index:
                 res_index = i
 
         return res[:res_index]
