01acef9b0596917213e92a02d10757fc5ff5785a
Emmanuel Emejuru <66489884+La-BeTe@users.noreply.github.com>
2025-03-25 01:35:16 +0100

    Time: 4 ms (11.49%), Space: 18 MB (8.46%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -5,18 +5,17 @@ class Solution:
         res_len = len(res)
 
         for str in strs[1:]:
+            i = 0
             str_len = len(str)
-            i, new_res_index = 0, 0
 
             while i < str_len and i < res_len and str[i] == res[i]:
                 i += 1
-                new_res_index = i
             
-            if new_res_index == 0:
+            if i == 0:
                 return ""
                 
-            if res_index is None or new_res_index < res_index:
-                res_index = new_res_index
+            if res_index is None or i < res_index:
+                res_index = i
 
         return res[:res_index]
         
